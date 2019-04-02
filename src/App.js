c3be474d79145ed3fa9d7bae54acadfb8ab71dc5
nramiscal <nramiscal@gmail.com>
2019-04-02 19:22:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Counter extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          value: 0,
+      }
+  }
+
+  increment() {
+      this.setState({value: this.state.value + 1});
+  }
+
+  decrement() {
+      this.setState({value: this.state.value -1});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div className="counter">
+            <h1 className="value">{this.state.value}</h1>
+            <button className="blue" onClick = {()=>{this.increment()}}>Increment</button>
+            <button className="blue" onClick = {()=>{this.decrement()}}>Decrement</button>
+          </div>
     );
   }
+}
+
+
+let counterId = 1;
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            counters: [0]
+        }
+    }
+
+    addCounter = () => {
+        let counters = [...this.state.counters];
+        counters.push(counterId);
+        counterId++;
+        this.setState({ counters: counters });
+    }
+
+    render () {
+        const { counters } = this.state;
+        return (
+            <div className="main">
+                <div className="header">
+                    <h1>Counter</h1>
+                </div>
+                <button className="topbar add" onClick={this.addCounter}>Add a Counter</button>
+                <div className="counter-list">
+                    { counters.map((id) => <Counter key={id}/>) }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
